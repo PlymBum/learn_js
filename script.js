41692b3deef0481f0cd172facc6ef128f28a0df6
PlymBum <sasikov.t.o@gmail.com>
2021-01-16 17:31:11 +0300

    interval

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('hello');
+let styleHeader = document.getElementById('header')
+styleHeader.style.textAlign = 'center';
+let rebuild = function(x) {
+
+    if (x.style.color == "red" && x.style.backgroundColor == "blue") {
+        x.style.color = "blue";
+        x.innerHTML = 'no Good Day!'
+        x.style.backgroundColor = "red";
+    } else {
+        x.style.color = "red";
+        x.innerHTML = 'Good Day!'
+        x.style.backgroundColor = "blue";
+    }
+}
+
+setInterval(() => {
+    rebuild(styleHeader)
+
+}, 1000);
