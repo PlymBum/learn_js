562ac161b7a3d96921f9bff50f88f46ca0794842
PlymBum <sasikov.t.o@gmail.com>
2021-03-20 19:01:16 +0300

    change pc

--- src/index.js
+++ src/index.js
@@ -1,34 +1,49 @@
 import Outpoot from "./outpoot";
 import '@styles/style.css'
+import bk from "./assets/bk.jpg"
+import mc from "./assets/mc.jpg"
 
 const outpoot = new Outpoot("Привет, я жив")
 console.log(outpoot.toString())
 
-const urlDb='https://js-learn-8a507-default-rtdb.firebaseio.com/'
 
-const tagline=document.getElementById('tagline');
-const img=document.getElementsByClassName('logo')
-img[0].style.backgroundImage='url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.malls.ru%2Frus%2Fnews%2Fmcdonald-s-razdelyaet-logotip-i-zakryvaet-restorany-v-evrope.shtml&psig=AOvVaw1e1TL2minnuwQtFFq5DXF-&ust=1615999343319000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDshemgte8CFQAAAAAdAAAAABAI");'
+const tagline = document.getElementById('tagline');
+//const img = document.getElementsByClassName('logo')
+//img[0].style.backgroundImage = 'url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.malls.ru%2Frus%2Fnews%2Fmcdonald-s-razdelyaet-logotip-i-zakryvaet-restorany-v-evrope.shtml&psig=AOvVaw1e1TL2minnuwQtFFq5DXF-&ust=1615999343319000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDshemgte8CFQAAAAAdAAAAABAI");'
 
-function chText (el,text1,text2){
-    el.innerHTML!=text1?el.innerHTML=text1:el.innerHTML=text2
+function chText(el, text1, text2) {
+    el.innerHTML != text1 ? el.innerHTML = text1 : el.innerHTML = text2
 }
 
 
-const changeTag=document.getElementById('tag')
-changeTag.addEventListener('click',()=>{
-    chText(tagline,'Жри бургеры!!!','Не жри бургеры!!!')
+const changeTag = document.getElementById('tag')
+changeTag.addEventListener('click', () => {
+    chText(tagline, 'Жри бургеры!!!', 'Не жри бургеры!!!')
     console.log(tagline.innerHTML)
+    tagline.insertAdjacentHTML("beforeend", html)
+    //chImage(0)
 
+})
 
-} )
+function chImage(counter) {
+    counter++
+    counter % 2 ? img[0].style.backgroundImage = mc : img[0].style.backgroundImage = bk
 
-let response =  fetch(urlDb);
-
-if (response.ok) { // если HTTP-статус в диапазоне 200-299
-                    // получаем тело ответа (см. про этот метод ниже)
-    console.log('good')
-     let json =  response.json();
-} else {
- console.log("Ошибка HTTP: " + response.status);
 }
+
+
+const html = `
+    </div>
+        <div>
+            <hr>
+            <button>
+                <a href="https:\\\\google.ru">
+            google.ru
+        </a>
+            </button>
+            <hr>
+            
+        
+        </div>
+`
+
